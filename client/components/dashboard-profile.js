8567ba535269737b21c526a7d060cada84fe92cb
Кирилюк <kk87.work@gmail.com>
2020-09-17 09:48:13 +0300

    fix profile

--- client/components/dashboard-profile.js
+++ client/components/dashboard-profile.js
@@ -7,8 +7,8 @@ const DashboardProfile = () => {
     <>
       <div id="title">Profile</div>
       <div id="username">{user}</div>
-      <Link to="/dashboard/profile/87c6394c-49c8-4872-9e89-db52ccf178ed">Go To Profile</Link>
       <Link to="/dashboard">Go To Root</Link>
+      <Link to="/dashboard/main">Go To Main</Link>
     </>
   )
 }
