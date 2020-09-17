5cd33a28cae5b9f4f68261a0586f2848ce2afff9
Кирилюк <kk87.work@gmail.com>
2020-09-17 09:28:21 +0300

    fix user params

--- client/components/dashboard-profile.js
+++ client/components/dashboard-profile.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 
 const DashboardProfile = () => {
-  const { username } = useParams()
+  const { user } = useParams()
   return (
     <>
       <div id="title">Profile</div>
-      <div id="username">{username}</div>
+      <div id="username">{user}</div>
       <Link to="/dashboard/profile/87c6394c-49c8-4872-9e89-db52ccf178ed">Go To Profile</Link>
       <Link to="/dashboard">Go To Root</Link>
     </>
